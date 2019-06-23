a7165ca91f9b0a73151c2a8d44aac8085a072901
Matt Williams <matt.seongjun@gmail.com>
2019-06-23 12:14:30 -0400

    Legit

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+function findMatching(array, string) {
+    return array.filter( name => name.toLowerCase() === string.toLowerCase() );
+};
+
+function fuzzyMatch(array, string) {
+    return array.filter( name => {
+        let stringInName = false;
+        for (let i = 0; i < string.length; ++i) {
+            if (string[i] === name[i])
+                stringInName = true;
+            else 
+                return false;
+        };
+        return stringInName;
+    });
+};
+
+function matchName(array, string) {
+    return array.filter( driver => driver.name.toLowerCase() === string.toLowerCase() );
+};
