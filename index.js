0ca0c4a498913609f42b0c7b0af30da418cebf68
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-06-23 19:01:54 -0400

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+
+let findMatching = function(drivers, str) {
+    let result = drivers.filter(driver => {
+        return driver.toUpperCase() === str.toUpperCase()
+    });
+
+    return result
+}
+
+let fuzzyMatch = function(drivers, str) {
+    let result = drivers.filter(driver => {
+        return driver.slice(0, str.length) === str
+    })
+
+    return result
+}
+
+let matchName = function(drivers, str) {
+    let result = drivers.filter(driver => driver.name === str)
+
+    return result
+}
