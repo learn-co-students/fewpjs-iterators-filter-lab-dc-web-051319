a65a972395e48ba88c2a87c397fa68628fbebe43
Joseph Arias <arias.joseph.a@gmail.com>
2019-06-23 10:11:28 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+
+function findMatching(array, name) {
+  return array.filter(person => person.toLowerCase() === name.toLowerCase())
+}
+
+function fuzzyMatch(drivers, letters) {
+  return drivers.filter(name => name.startsWith(letters))
+}
+
+function matchName(drivers, name) {
+  return drivers.filter(function(driver_obj) {
+    return driver_obj.name === name
+  })
+}
