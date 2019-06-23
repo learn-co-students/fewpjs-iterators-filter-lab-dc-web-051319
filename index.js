68c8c4284fd21fc0ea1d7790f899b2ccf3085c21
newtypeV2 <gianlibunao@gmail.com>
2019-06-23 12:18:24 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(array,string){
+    return array.filter(name => {return name.toLowerCase() == string.toLowerCase();});
+}
+
+function fuzzyMatch(array,string){
+    return array.filter(name => {return (name.includes(string) && name.indexOf(string)==0);});
+}
+
+function matchName(array,string){
+    return array.filter(record => {return (record.name==string);});
+}
