7381ba27396fbc234c356ba784d5913e985a4a56
keithburgie <krburgie@gmail.com>
2019-06-23 22:37:30 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+import { listenerCount } from "cluster";
+
 // Code your solution here
+function findMatching(drivers, searchTerm) {
+  return drivers.filter((driver) => {
+    return driver.toLowerCase() === searchTerm.toLowerCase()
+  });
+}
+
+function fuzzyMatch(drivers, searchTerm) {
+  let lengthOfName = searchTerm.length;
+  return drivers.filter((driver) => {
+    return driver.slice(0, lengthOfName) == searchTerm;
+  });
+}
+
+function matchName(drivers, searchTerm) {
+  return drivers.filter((driver) => {
+    return driver.name.toLowerCase() === searchTerm.toLowerCase();
+  });
+}
